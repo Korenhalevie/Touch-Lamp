6b812a59861b7898bc844c7e903f637a1d8e12fc
Koren Halevie <koren0011@gmail.com>
2024-09-05 23:10:30 +0300

    added sleep mode and its settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,67 @@
 #include <Arduino.h>
 #include "WS2812.h"
+#include "driver/touch_pad.h" // to wake up from touch
 
 #define LED_PIN GPIO_NUM_18
-#define TOUCH_PIN GPIO_NUM_4
+#define TOUCH_PIN T0 // T0 = GPIO 4
 #define RMT_CHANNEL RMT_CHANNEL_0
+#define TOUCH_THRESHOLD 20
 
-
-int touchValue;
-int ledState = LOW; // start with LED off
-bool isTouched = false; // flag to check if the touch sensor is touched
-
+// because we are in deep sleep, ram is off, so we need to use RTC_DATA_ATTR that stores the variable in RTC memory
+RTC_DATA_ATTR bool ledState = LOW;
+RTC_DATA_ATTR bool isTouchHeld = false;
 
 WS2812 led(LED_PIN, RMT_CHANNEL);
-// WS2812 led(LED_PIN, RMT_CHANNEL, 255, 0, 0); // red
-// WS2812 led(LED_PIN, RMT_CHANNEL, 0, 255, 0); // green
-// WS2812 led(LED_PIN, RMT_CHANNEL, 0, 0, 255); // blue
 
-// uint8_t currentRed, currentGreen, currentBlue; // uncomment this line if you want to keep the same color
-
-void setup() {
-    led.setupRMT();
-    // led.getColor(currentRed, currentGreen, currentBlue); // get current color, uncomment this line if you want to keep the same color
+void goToDeepSleep()
+{
+  touchAttachInterrupt(TOUCH_PIN, [] {}, TOUCH_THRESHOLD);
+  esp_sleep_enable_touchpad_wakeup();
+  esp_deep_sleep_start();
 }
 
-void loop() {
-    touchValue = touchRead(TOUCH_PIN);
+void setup()
+{
+  led.setupRMT();
 
-
-    if (touchValue < 40) { // Touch sensor is activated
-        if (!isTouched) { 
-            ledState = !ledState; // Toggle the LED state
-            
-            if (ledState) { // If LED is on
-                uint8_t currentRed = random(0, 256);
-                uint8_t currentGreen = random(0, 256);
-                uint8_t currentBlue = random(0, 256);
-            } else { // If LED is off
-                led.setColor(0, 0, 0);
-            }
-            led.lightPixels(); 
-            isTouched = true; 
-    } else {
-        isTouched = false;
+  if (esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TOUCHPAD)
+  { // check if touchpin held and it wakes up
+    if (!isTouchHeld)
+    {
+      isTouchHeld = true;
+      ledState = !ledState;
+      if (ledState)
+      {
+        uint8_t currentRed = random(0, 256);
+        uint8_t currentGreen = random(0, 256);
+        uint8_t currentBlue = random(0, 256);
+        led.setColor(currentRed, currentGreen, currentBlue);
+      }
+      else
+      {
+        led.setColor(0, 0, 0);
+      }
+      led.lightPixels();
     }
+  }
+  else
+  { // the system starts ehre
+    led.setColor(0, 0, 0);
+    led.lightPixels();
+  }
+
+  bool touchReleased = false;
+  while (!touchReleased)
+  {
+    if (touchRead(TOUCH_PIN) > 20)
+    {
+      touchReleased = true;
+
+      goToDeepSleep();
+    }
+  }
 }
+
+void loop()
+{
 }
